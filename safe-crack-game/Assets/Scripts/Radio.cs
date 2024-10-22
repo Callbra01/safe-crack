c00339c935d217340194f6aa4d38f6468c8e0aba
wightkid <103604852+wightkid@users.noreply.github.com>
2024-10-21 23:36:07 -0400

    Core Radio Functionality
    Add Static audio
    Add test number audio
    Add manual freq sweeping
    Add automatic static volume adjustment based on position to target frequency
    
    TODO:
    ADD LIGHTS AND LIGHT FUNCTIONALITY
    ADD SINE TO ADJUST TARGET FREQUENCY
    ADD VOCAL TRACK ONE HITS
    MODIFY VOCAL TRACK VOLUME TO OPPOSITE OF STATIC VOLUME
    ADD CHECK FOR WIN / LOSE STATE
    ADD BUTTON FOR FREQUENCY SELECTION
    ADD CHECK FOR CORRECT AND WRONG SELECTION

--- safe-crack-game/Assets/Scripts/Radio.cs
+++ safe-crack-game/Assets/Scripts/Radio.cs
@@ -5,46 +5,74 @@ using UnityEngine;
 /*
  * BASIC RADIO CLASS, HAS BASIC MOVEMENT
  * TODO: ADD PHYSICAL SWITCH INPUT AND MANIPULATE NEEDLE POSITION BASED ON THAT NUMBER
- * */
+ * TODO: QUANITFY FREQUENCY NEEDLE MOVEMENT TO BETTER SYNC  
+ */
 public class Radio : MonoBehaviour
 {
     public Transform frequencyNeedleTransform;
 
-    float currentValue;
+    float currentFreqValue;
     public float maxFreqValue = 2.5f;
     public float minFreqValue = 0f;
     public float tuneSpeed = 0.15f;
 
+    public float targetFrequency = 1f;
 
-    void Setup()
-    {
-        currentValue = frequencyNeedleTransform.position.x;
-    }
+    public AudioSource staticSource;
+    public float staticClipMaxVolume = 0.4f;
 
 
     void Update()
     {
         HandleInput();
+        HandleAudio();
     }
 
+    // TODO: UPDATE INPUT FROM Q AND E TO LINEAR SLIDER
     void HandleInput()
     {
         if (Input.GetKeyDown(KeyCode.Q))
         {
-            if (currentValue > minFreqValue)
+            if (currentFreqValue > minFreqValue)
             {
                 frequencyNeedleTransform.Translate(-tuneSpeed, 0, 0);
-                currentValue -= tuneSpeed;
+                currentFreqValue -= tuneSpeed;
             }
             
         }
         else if (Input.GetKeyDown(KeyCode.E))
         {
-            if (currentValue < maxFreqValue)
+            if (currentFreqValue < maxFreqValue)
             {
                 frequencyNeedleTransform.Translate(tuneSpeed, 0, 0);
-                currentValue += tuneSpeed;
+                currentFreqValue += tuneSpeed;
             }
+        }
+    }
+
+    // TODO: ADD SINE WAVE TO AUTOMATICALLY ADJUST TARGET FREQ
+    // Changes the target frequency to a given frequency between 0.1f, and 2.5f
+    void UpdateTargetFrequency(float newFrequency)
+    {
+        targetFrequency = newFrequency;
+    }
+
+    // TODO: ADD VOCAL TRACK ONE SHOTS AND SET VOLUME TO OPPOSITE OF STATIC VOLUME
+    void HandleAudio()
+    {
+        // Get the difference between current freq and tar freq, adjust volume accordingly
+        float freqDifference = Mathf.Abs(currentFreqValue - targetFrequency);
+ 
+        staticSource.volume = (freqDifference * 0.5f);
+
+        // Clamp static volume to a minimum and maximum volume
+        if (staticSource.volume > staticClipMaxVolume)
+        {
+            staticSource.volume = staticClipMaxVolume;
+        }
+        else if (staticSource.volume < 0.1f)
+        {
+            staticSource.volume = 0.1f;
         }
     }
 }
